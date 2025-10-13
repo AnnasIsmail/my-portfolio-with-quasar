5e64b28abab2d547e40dab061f892c43efc22843
gconn123431 <fadhilahrizqi.aiiso@gmail.com>
2025-10-13 15:43:28 +0700

    Update Netlify build command to include npm install and adjust TypeScript ESLint configuration based on development mode.

--- quasar.config.js
+++ quasar.config.js
@@ -18,7 +18,7 @@ module.exports = configure(function (ctx) {
     supportTS: {
       tsCheckerConfig: {
         eslint: {
-          enabled: true,
+          enabled: ctx.dev,
           files: './src/**/*.{ts,tsx,js,jsx,vue}'
         }
       }
